91cbd329783d2603a68f2e3a0e09dd0ec59ec731
L200164008 <ajiwoke@gmail.com>
2018-12-01 17:44:16 +0700

    Delete hasillogin.php

--- modul8/hasillogin.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	echo "Anda telah login sebagai ". $_session['username']."dan anda terdaftar sebagai ".$_session['status'];
-?>
-<br>
-Silahkan logout dengan klik link <a href="logout.php"> disini </a>
